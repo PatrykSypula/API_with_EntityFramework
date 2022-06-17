176b9e952048b9b0634dc62463038125e7079e0a
Patryk Sypuła <UL0253548@edu.uni.lodz.pl>
2022-06-17 12:07:52 +0200

    Create

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+
+        private static List<Person> people = new List<Person>
+            {
+                new Person {
+                    Id = 1,
+                    FirstName ="Patryk",
+                    LastName ="Sypuła",
+                    Place="Łódź" },
+                new Person {
+                    Id = 2,
+                    FirstName ="Patrick",
+                    LastName ="Sypula",
+                    Place="Lodz" }
+            };
+
+        [HttpGet]//Show all people
+        public async Task<ActionResult<List<Person>>> Get()
+        {
+            return Ok(people);
+        }
+
+        [HttpGet("{id}")]//Show particular person by id
+        public async Task<ActionResult<Person>> Get(int id)
+        {
+            var person = people.Find(p => p.Id == id);
+            if (person == null)
+            {
+                return BadRequest("Person not found.");
+            }
+            else
+            {
+                return Ok(person);
+            }
+        }
+
+        [HttpPost]//Add person
+        public async Task<ActionResult<List<Person>>> AddPerson(Person person)
+        {
+            people.Add(person);
+            return Ok(people);
+        }
+
+        [HttpPut]//Update person
+        public async Task<ActionResult<List<Person>>> UpdatePerson(Person request)
+        {
+            var person = people.Find(p => p.Id == request.Id);
+            if (person == null)
+            {
+                return BadRequest("Person not found.");
+            }
+            else
+            {
+                person.FirstName = request.FirstName;
+                person.LastName = request.LastName;
+                person.Place = request.Place;
+            }
+            return Ok(people);
+        }
+
+        [HttpDelete("{id}")]//Delete particular person by id
+        public async Task<ActionResult<List<Person>>> Delete(int id)
+        {
+            var person = people.Find(p => p.Id == id);
+            if (person == null)
+            {
+                return BadRequest("Person not found.");
+            }
+            else
+            {
+                people.Remove(person);
+                return Ok(people);
+            }
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string Place { get; set; } = String.Empty;
+
+    }
+}
